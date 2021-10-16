604f34bb89799e370f7b7970e9e486dc53133f64
Paul Forbes <paulforbes2k9@hotmail.co.uk>
2021-10-16 19:33:49 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#week 1 practical
+
+#task 1
+
+#user enters their height
+
+height = input('Enter your height in cm: ')
+
+#print the users height
+
+print('Your height is: ', height, 'cm')
+
+#task 2
+
